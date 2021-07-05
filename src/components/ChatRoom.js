188ebd478dae49bea89ec833239ca09bb907dac0
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-07-05 21:22:07 +0200

    feat: messages

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -9,14 +9,55 @@ import ChatMessage from './ChatMessage';
 
 function ChatRoom() {
   const { firebase, auth, firestore } = useContext(FirebaseContext);
+  const { muted } = useContext(UIContext);
   const [formValue, setFormValue] = useState('');
   const bottom = useRef();
-  const [messages, setMessages] = useState([]); // TODO: implement
+  // const [messages, setMessages] = useState([]); // TODO: implement
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt'); // .limit(25)
+  const [messages] = useCollectionData(query, { idField: 'id'});
+
+  const [playSound] = useSound(
+    '/sounds/wiadomosc.mp3',
+    { volume: 0.25 }
+  );
+
+  const watchUsers = async () => {
+    firebase.firestore().collection('messages')
+    .onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'added') {
+          const { uid: docUid } = change.doc.data();
+          const { uid } = auth.currentUser;
+          if (!muted && docUid !== uid) {
+            playSound();
+          }
+        }
+      })
+    })
+  }
+
+  useEffect(() => {
+    watchUsers();
+  }, []);
 
   const sendMessage = async(event) => {
     event.preventDefault();
 
      // TODO: implement
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue('');
+    if (bottom.current) {
+      bottom.current.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   return (
